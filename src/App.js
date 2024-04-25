dac681e21d272a8f281e073fd7d2203934eee3e0
Nico De Castro <jtdecastro@up.edu.ph>
2024-04-25 21:48:45 +0800

    Complete week 8 handout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import SubjectList from "./SubjectList";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      Hello World
+      <SubjectList data={CMSCSubjects} title={'Computer Science Subjects'} />
+      <SubjectList data={CHEMSubjects} title={'Chemistry'} />
     </div>
   );
 }
+
+const CMSCSubjects = [
+  { code: "CMSC100", description: "Web Programming", id: 1 },
+  { code: "CMSC150", description: "Scientific Computation", id: 2 },
+  { code: "CMSC22", description: "Object-Oriented Programming", id: 3 },
+];
+
+const CHEMSubjects = [
+  { code: "CHEM100", description: "Introduction to Organic Chemistry", id: 1 },
+  { code: "CHEM10", description: "Chemical Kinetics", id: 2 },
+  { code: "CHEM11", description: "Introduction to Biochemistry", id: 3 },
+];
 
 export default App;
